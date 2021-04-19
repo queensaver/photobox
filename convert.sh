d96d429e98be0b3cd7063e5f5abc8c1e7e2871dd
Wolfgang Hennerbichler <wogri@github.com>
2021-04-19 08:40:46 +0200

    source link convert

--- convert.sh
+++ convert.sh
@@ -1,1 +1,2 @@
- convert tmp/image.jpg -virtual-pixel black -distort Barrel "0.0 0.0 -0.37 1.5" tmp/image_out.jpg && open tmp/image_out.jpg
+# https://legacy.imagemagick.org/Usage/distorts/#barrel
+convert tmp/image.jpg -virtual-pixel black -distort Barrel "0.0 0.0 -0.37 1.5" tmp/image_out.jpg && open tmp/image_out.jpg
